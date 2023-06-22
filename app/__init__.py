da63a318a86125304e271f9bf8721669a5d124ec
Fill <tima.filicheff1@yandex.ru>
2023-06-22 22:17:50 +0300

    Создание приложения, подключение БД и регистрация Blueprint

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+application = Flask(__name__)
+application.secret_key = 'some_very_strong_secret_key'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/testdb'
+db = SQLAlchemy(application)
+application.app_context().push()
+
+from app.auth.views import auth as blueprint_auth
+
+application.register_blueprint(blueprint_auth)
