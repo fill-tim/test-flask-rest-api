538c8d1afdc1871f08848433df07fb665696fb23
Fill <tima.filicheff1@yandex.ru>
2023-06-24 20:02:21 +0300

    ДобавиÑл полю username свойство unique=True

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,3 +1,4 @@
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
@@ -5,9 +6,9 @@ from app.friends.models import friendship
 from app.friends.serializers import FriendListSerializer
 
 
-class User(db.Model, FriendListSerializer):
+class User(db.Model, FriendListSerializer, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
