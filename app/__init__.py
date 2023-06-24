323d3f948660ed3b5f7c52b4af40f3a2ed723856
Fill <tima.filicheff1@yandex.ru>
2023-06-24 20:38:55 +0300

    добазагрегистрировалс Blueprint profile

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
 application.secret_key = 'some_very_strong_secret_key'
 application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/testdb'
 db = SQLAlchemy(application)
+migrate = Migrate(application, db)
 
 application.app_context().push()
 
 from app.auth.views import auth as blueprint_auth
 from app.friends.views import friend as blueprint_friend
+from app.profile.views import profile
 
 application.register_blueprint(blueprint_auth)
+application.register_blueprint(profile)
 application.register_blueprint(blueprint_friend)
