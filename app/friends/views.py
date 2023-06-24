5752c2f64449795397b0d8023957bd4f07681ddb
Fill <tima.filicheff1@yandex.ru>
2023-06-24 16:22:56 +0300

    Метод вывода списка друзей пользователя по выбранному id

---
+++ app/friends/views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify, request
+
+from app.auth.models import User
+
+friend = Blueprint('blueprint_friend', __name__)
+
+
+@friend.route('/print', methods=["POST"])
+# @login_required
+def print_text():
+    """ Список друзей пользователя """
+    data = request.get_json()
+    user = User.query.filter_by(id=data.get('id')).first()
+    friends_dict = [obj.serialize() for obj in user.friends]
+
+    return jsonify(friends_dict)
