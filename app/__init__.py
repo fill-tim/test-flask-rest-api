2ed3f9d5d54d282075107ae9b2f978ea51a2f98b
Fill <tima.filicheff1@yandex.ru>
2023-06-24 16:24:07 +0300

    Добавил Blueprint friends

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,11 @@ application = Flask(__name__)
 application.secret_key = 'some_very_strong_secret_key'
 application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/testdb'
 db = SQLAlchemy(application)
+
 application.app_context().push()
 
 from app.auth.views import auth as blueprint_auth
+from app.friends.views import friend as blueprint_friend
 
 application.register_blueprint(blueprint_auth)
+application.register_blueprint(blueprint_friend)
